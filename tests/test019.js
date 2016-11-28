8464ad5fef23ba43e9fd50464a1c19b0bc2a5a6c
Guillermo Hernán <guillermo.hernan.martin@gmail.com>
2016-11-28 18:57:24 +0100

    'clone' is not a standard Javascript function. Removed from test.

--- tests/test019.js
+++ tests/test019.js
@@ -12,8 +12,11 @@ obj1 = new Object();
 obj1.food = "cake";
 obj1.desert = "pie";
 
-obj2 = obj1.clone();
-obj2.food = "kittens";
+obj2 = obj1;
+//obj2 = obj1.clone();
+//obj2.food = "kittens";
 
 result = foo.length==13 && foo.indexOf("bar")==4 && foo.substring(8,13)=="stuff" && parsed==42 && 
          Integer.valueOf(aChar)==65 && obj1.food=="cake" && obj2.desert=="pie";
+
+result = foo.length==13;
