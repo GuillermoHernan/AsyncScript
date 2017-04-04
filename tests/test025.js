9284844adc7b87494d192efe7c0100a515a52781
Guillermo Hernán <guillermo.hernan.martin@gmail.com>
2017-04-04 23:02:50 +1200

    Test modified to use 'asserts'.

--- tests/test025.js
+++ tests/test025.js
@@ -4,4 +4,7 @@ var myArray = [ 1, 2, 3, 4, 5 ];
 var myArray2 = [ 1, 2, 3, 4, 5 ];
 myArray2[8] = 42;
 
-result = myArray.length == 5 && myArray2.length == 9;
+assert (myArray.length == 5, "'myArray' length: " + myArray.length);
+assert (myArray2.length == 9, "'myArray2' length: " + myArray2.length);
+
+result = 1;
