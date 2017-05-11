0cddf22bb68647e638bfaa25e5e3d686ca34617f
Guillermo Hernán <guillermo.hernan.martin@gmail.com>
2017-05-11 23:55:40 +1200

    Test rewritten to get more precise error location.

--- tests/test031.js
+++ tests/test031.js
@@ -2,4 +2,9 @@
 var b = "1,4,7";
 var a = b.split(",");
 
-result = a.length==3 && a[0]==1 && a[1]==4 && a[2]==7;
+assert (a.length == 3, "a.length = " + a.length);
+assert (a[0]==1, "a[0]=" + a[0]); 
+assert (a[1]==4, "a[1]=" + a[1]); 
+assert (a[2]==7, "a[2]=" + a[2]);
+
+result = 1;
